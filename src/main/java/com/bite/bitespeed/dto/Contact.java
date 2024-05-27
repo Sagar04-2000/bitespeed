204fd8f04e5eafadd5d43a0d09c954d07cbc5328
Sagar04-2000 <sagarwagh2426@gmail.com>
2024-05-27 15:22:12 +0530

    contact response modified

--- src/main/java/com/bite/bitespeed/dto/Contact.java
+++ src/main/java/com/bite/bitespeed/dto/Contact.java
@@ -1,6 +1,48 @@
 package com.bite.bitespeed.dto;
 
+import java.util.List;
+import java.util.Set;
+
 public class Contact {
 
+    private Long primaryContactId;
+
+    private Set<String> emails;
+
+    private Set<String> phoneNumbers;
+
+    private List<Long> secondaryContactIds;
+
+    public Long getPrimaryContactId() {
+        return primaryContactId;
+    }
+
+    public void setPrimaryContactId(Long primaryContactId) {
+        this.primaryContactId = primaryContactId;
+    }
+
+    public Set<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(Set<String> emails) {
+        this.emails = emails;
+    }
+
+    public Set<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(Set<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public List<Long> getSecondaryContactIds() {
+        return secondaryContactIds;
+    }
+
+    public void setSecondaryContactIds(List<Long> secondaryContactIds) {
+        this.secondaryContactIds = secondaryContactIds;
+    }
 
 }
