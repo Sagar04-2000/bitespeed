b30487016fd69480a2540ad1dc82ac49cc15dc36
Sagar04-2000 <sagarwagh2426@gmail.com>
2024-05-29 15:40:03 +0530

    Docker File and Docker Compose file added

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,7 @@ LABEL maintainer="sagar-bitespeed"
 ##ADD jar file
 ADD target/bitespeed-0.0.1-SNAPSHOT.jar bitespeed-docker.jar
 
+#COPY target/bitespeed-0.0.1-SNAPSHOT.jar bitespeed-docker.jar
+
 ##Adding entrypoint
-ENTRYPOINT["java","-jar","bitespeed-docker.jar"]
+ENTRYPOINT ["java","-jar","/bitespeed-docker.jar"]
--- src/main/java/com/bite/bitespeed/controller/CustomerController.java
+++ src/main/java/com/bite/bitespeed/controller/CustomerController.java
@@ -8,10 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/customer")
 @RestController
@@ -29,6 +26,11 @@ public class CustomerController {
     public ResponseEntity<CustomerContactResponse> identifyCustomerContact(@RequestBody CustomerContactRequest customerContactRequest){
         logger.info("CUSTOMER_CONTROLLER_STARTED >>>");
         return customerContactMappingService.identifyCustomerContact(customerContactRequest);
+    }
+
+    @GetMapping("/testEndpoint")
+    public String testEndPoint(){
+        return " Test Done";
     }
 }
 
